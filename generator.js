92933d1a5cd548c2e8bd50098c872b0ab6c0db77
doowb <brian.woodward@gmail.com>
2016-08-26 10:26:05 -0400

    add tasks for index templates

--- generator.js
+++ generator.js
@@ -1,11 +1,14 @@
 'use strict';
 
 var path = require('path');
+var through = require('through2');
 var isValid = require('is-valid-app');
 
 module.exports = function(app) {
   // return if the generator is already registered
   if (!isValid(app, 'generate-webtask')) return;
+
+  app.use(require('generate-defaults'));
 
   /**
    * Generate a `index.js` file to the current working directory. Learn how to [customize
@@ -18,7 +21,13 @@ module.exports = function(app) {
    * @api public
    */
 
-  task(app, 'webtask', 'index.js');
+  task(app, 'webtask-simple', 'index_simple.js');
+  task(app, 'webtask-context', 'index_context.js');
+  task(app, 'webtask-http', 'index_http.js');
+  app.task('webtask', ['webtask-simple']);
+  app.task('simple', ['webtask-simple']);
+  app.task('context', ['webtask-context']);
+  app.task('http', ['webtask-http']);
 
   /**
    * Alias for running the [webtask](#webtask) task with the following command:
@@ -37,11 +46,23 @@ module.exports = function(app) {
  * Create a task with the given `name` and glob `pattern`
  */
 
-function task(app, name, pattern) {
-  app.task(name, function() {
-    return app.src(pattern, {cwd: __dirname})
-      .pipe(app.renderFile('*'))
-      .pipe(app.conflicts(app.cwd))
-      .pipe(app.dest(app.cwd));
+function task(app, name, pattern, dependencies) {
+  app.task(name, dependencies || [], function(cb) {
+    if (!pattern) return cb();
+    return file(app, pattern);
   });
+}
+
+function file(app, pattern) {
+  var src = app.options.srcBase || path.join(__dirname, 'templates');
+  return app.src(pattern, {cwd: src})
+    .pipe(app.renderFile('*')).on('error', console.log)
+    .pipe(through.obj(function(file, enc, next) {
+      if (file.stem.indexOf('index_') === 0) {
+        file.stem = 'index';
+      }
+      next(null, file);
+    }))
+    .pipe(app.conflicts(app.cwd))
+    .pipe(app.dest(app.cwd));
 }
